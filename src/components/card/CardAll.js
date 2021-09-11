cadb942c1652d9adaec6a95f5089a5a7c55e2409
Alex-der13 <76278871+Alex-der13@users.noreply.github.com>
2021-09-11 20:34:50 +0300

    add redux

--- src/components/card/CardAll.js
+++ src/components/card/CardAll.js
@@ -1,47 +1,28 @@
 import React from 'react';
 import { Bookmark } from '@material-ui/icons';
 import { Button } from '@material-ui/core';
-import './Card.css';
+import styles from './Card.module.scss';
 
 const CardAll = ({ addBasket, isInBasket, changeFavoriteList, category, isInFavorite }) => {
     const { Name, Params:{Author}, Price, PictureUrl } = category;
 
+    const handleAddToBasket = () => {
+      addBasket(category);
+    };
+
     return (
-        <div
-            style={{
-                margin: '50px 10px 10px',
-                width: '160px',
-                height: '360px',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-            }}
-        >
-            <div
-                style={{
-                    marginBottom: '10px',
-                    width: '130px',
-                    height: '200px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'flex-end',
-                }}
-            >
-                <img src={PictureUrl} alt="book" style={{ margin: '0 auto' }} />
+        <div className={styles.Card}>
+            <div className={styles.img_block}>
+                <img src={PictureUrl} alt="book" />
             </div>
-            <div className="card_title">{Name}</div>
+            <div className={styles.card_title}>{Name}</div>
             <div style={{ color: '#9c9c9c' }}>{Author}</div>
             <div style={{ fontWeight: 'bold' }}>{Price} ₽</div>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                }}
-            >
-                <Button disabled={isInBasket} className='card_buy_btn' variant="contained" color="primary" onClick={() => addBasket(category)}>
-                    {isInBasket? 'В корзине':'Купить' }
+            <div className={styles.buttons_block}>
+                <Button disabled={isInBasket} variant="contained" color="primary" onClick={handleAddToBasket}>
+                    {isInBasket ? 'В корзине' : 'Купить' }
                 </Button>
-                <div className="card_favorite_btn" onClick={() => changeFavoriteList(category)}>
+                <div className={styles.card_favorite_btn} onClick={() => changeFavoriteList(category)}>
                     <Bookmark color={isInFavorite ? 'primary' : 'disabled'} />
                 </div>
             </div>
---
+++ src/store/reducers/books.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    data: [],
+    loading: false,
+    error: false
+}
+
+export default createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    fetchBooks: (state) => {
+        state.loading = true
+    },
+    fetchBooksSuccess: (state, action) => {
+        state.loading = false
+        state.data = action.payload
+    },
+    fetchBooksError: (state) => {
+        state.loading = false
+        state.error = true
+    },
+  },
+})
